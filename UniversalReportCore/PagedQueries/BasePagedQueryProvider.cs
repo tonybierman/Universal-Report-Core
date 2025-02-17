e05e2f4d89c5d45515f14563b6e39b43c98c4980
tonybierman <tonybierman@gmail.com>
2025-02-17 13:47:50 -0600

    migating to new aggregation model

--- UniversalReportCore/PagedQueries/BasePagedQueryProvider.cs
+++ UniversalReportCore/PagedQueries/BasePagedQueryProvider.cs
@@ -13,7 +13,114 @@ namespace ProductionPlanner.PagedQueries
         {
         }
 
-        protected abstract Task<Dictionary<string, dynamic>> ComputeAggregates(IQueryable<T> query, IReportColumnDefinition[] columns);
+        protected virtual async Task<Dictionary<string, dynamic>> ComputeAggregates(IQueryable<T> query, IReportColumnDefinition[] columns)
+        {
+            var aggregateResults = new Dictionary<string, dynamic>();
+
+            foreach (var column in columns)
+            {
+                var propertyName = column.PropertyName;
+
+                // Ensure the property exists on the ProductInventory model
+                var property = typeof(T).GetProperty(propertyName);
+                if (property == null)
+                {
+                    continue;
+                }
+
+                // Handle the aggregation logic based on the AggregationType
+                switch (column.Aggregation)
+                {
+                    case AggregationType.Sum:
+                        aggregateResults[propertyName] = await query.SumAsync(x => EF.Property<decimal?>(x, propertyName)) ?? 0;
+                        break;
+
+                    case AggregationType.Average:
+                        aggregateResults[propertyName] = await query.AverageAsync(x => EF.Property<decimal?>(x, propertyName)) ?? 0;
+                        break;
+
+                    case AggregationType.Min:
+                        aggregateResults[propertyName] = await query.MinAsync(x => EF.Property<decimal?>(x, propertyName)) ?? 0;
+                        break;
+
+                    case AggregationType.Max:
+                        aggregateResults[propertyName] = await query.MaxAsync(x => EF.Property<decimal?>(x, propertyName)) ?? 0;
+                        break;
+
+                    case AggregationType.Count:
+                        aggregateResults[propertyName] = await query.CountAsync(x => EF.Property<object>(x, propertyName) != null);
+                        break;
+
+                    case AggregationType.None:
+                    default:
+                        // If no aggregation is needed, you can return the original value or leave it out
+                        aggregateResults[propertyName] = null;
+                        break;
+                }
+            }
+
+            return aggregateResults;
+        }
+
+        //protected abstract Task<Dictionary<string, dynamic>> ComputeAggregates(IQueryable<T> query, IReportColumnDefinition[] columns);
+        //protected override async Task<Dictionary<string, dynamic>> ComputeAggregates(
+        //IQueryable<ProductInventory> query,
+        //IReportColumnDefinition[] columns)
+        //    {
+        //        var aggregates = new Dictionary<string, dynamic>();
+
+        //        foreach (var column in columns)
+        //        {
+        //            var property = typeof(ProductInventory).GetProperty(column.PropertyName);
+        //            if (property == null) continue; // Skip if property doesn't exist
+
+        //            switch (column.Aggregation)
+        //            {
+        //                case AggregationType.Sum:
+        //                    aggregates[column.PropertyName] = await query.SumAsync(x =>
+        //                        (decimal?)property.GetValue(x) ?? 0);
+        //                    break;
+
+        //                case AggregationType.Average:
+        //                    aggregates[column.PropertyName] = await query.AverageAsync(x =>
+        //                        (decimal?)property.GetValue(x) ?? 0);
+        //                    break;
+
+        //                case AggregationType.Min:
+        //                    aggregates[column.PropertyName] = await query.MinAsync(x =>
+        //                        (decimal?)property.GetValue(x) ?? decimal.MaxValue);
+        //                    break;
+
+        //                case AggregationType.Max:
+        //                    aggregates[column.PropertyName] = await query.MaxAsync(x =>
+        //                        (decimal?)property.GetValue(x) ?? decimal.MinValue);
+        //                    break;
+
+        //                case AggregationType.Count:
+        //                    aggregates[column.PropertyName] = await query.CountAsync(x =>
+        //                        property.GetValue(x) != null);
+        //                    break;
+
+        //                case AggregationType.None:
+        //                default:
+        //                    aggregates[column.PropertyName] = "N/A"; // No aggregation
+        //                    break;
+        //            }
+        //        }
+
+        //        return aggregates;
+        //    }
+
+
+        //protected override async Task<Dictionary<string, dynamic>> ComputeAggregates(IQueryable<ProductInventory> query, IReportColumnDefinition[] columns)
+        //{
+        //    return new Dictionary<string, dynamic>
+        //    {
+        //        { "InventorySupplyAtFba", await query.SumAsync(x => x.InventorySupplyAtFba) },
+        //        { "SuggestedInventoryLevel", await query.SumAsync(x => x.SuggestedInventoryLevel) },
+        //    };
+        //}
+
         protected abstract Task<Dictionary<string, dynamic>> EnsureMeta(IQueryable<T> query);
         protected virtual IQueryable<T> ApplyFilters(IQueryable<T> query)
         {
