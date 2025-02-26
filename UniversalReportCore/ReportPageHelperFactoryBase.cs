cf927ebc5df54cdca8d372ff7fc6352170b61c38
tonybierman <tonybierman@gmail.com>
2025-02-25 19:15:09 -0600

    ReportPageHelperFactoryBase

---
+++ UniversalReportCore/ReportPageHelperFactoryBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalReportCore
+{
+    public class ReportPageHelperFactoryBase
+    {
+        protected readonly IServiceProvider _serviceProvider;
+        protected readonly Dictionary<string, (Type EntityType, Type ViewModelType)> _helperMap = new Dictionary<string, (Type EntityType, Type ViewModelType)>();
+
+        public ReportPageHelperFactoryBase(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IReportPageHelperBase GetHelper(string reportType)
+        {
+            if (!_helperMap.TryGetValue(reportType, out var types))
+            {
+                throw new ArgumentException($"No PageHelper found for report type {reportType}");
+            }
+
+            var helperType = typeof(IReportPageHelper<,>).MakeGenericType(types.EntityType, types.ViewModelType);
+            var helperInstance = _serviceProvider.GetService(helperType);
+
+            return helperInstance as IReportPageHelperBase
+                ?? throw new InvalidOperationException($"Failed to resolve {helperType.Name}");
+        }
+    }
+}
