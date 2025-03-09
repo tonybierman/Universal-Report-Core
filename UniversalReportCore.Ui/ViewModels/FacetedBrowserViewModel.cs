42c82a74ba5104f27b00f5139d1b9782266de92a
tonybierman <tonybierman@gmail.com>
2025-03-09 11:32:56 -0500

    Faceted Browser moved to partial

---
+++ UniversalReportCore.Ui/ViewModels/FacetedBrowserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversalReportCore.HardQuerystringVariables;
+
+namespace UniversalReportCore.Ui.ViewModels
+{
+    public class FacetedBrowserViewModel
+    {
+        public IReportQueryParams Params { get; set; }
+
+        public List<(string Heading, List<SelectListItem> Options)> FilterOptions { get; set; }
+
+        public int? PageIndex { get; set; }
+
+        public string? SortOrder { get; set; }
+    }
+}
