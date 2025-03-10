030e0fb3ecc6da3dedcf6573e35192180f900332
tonybierman <tonybierman@gmail.com>
2025-03-09 19:18:52 -0500

    Fixed broken test

--- UniversalReportCore.Tests/PagedQueryParametersTests.cs
+++ UniversalReportCore.Tests/PagedQueryParametersTests.cs
@@ -89,7 +89,7 @@ namespace UniversalReportCore.Tests
             var queryParameters = new PagedAcmeQueryParameters(_columns, 1, "Population", 10, null, filterLogic, null, null);
 
             // Act
-            var filteredResult = queryParameters.UserFilter!(data).ToList();
+            var filteredResult = queryParameters.ReportFilter!(data).ToList();
 
             // Assert
             Assert.NotNull(filteredResult);
