c0e53f77d8e46d5339c904928fb74534a2e497b6
tonybierman <tonybierman@gmail.com>
2025-03-19 05:45:10 -0500

    JsonHelper

---
+++ UniversalReportCore/Helpers/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace UniversalReportCore.Helpers
+{
+    public static class JsonHelper
+    {
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, _options);
+        }
+
+        public static string Serialize<T>(T value)
+        {
+            return JsonSerializer.Serialize(value, _options);
+        }
+    }
+}
