19da6e596f23e54551ce3b854e10ca46f155a82d
tonybierman <tonybierman@gmail.com>
2025-03-18 19:42:29 -0500

    Implementing command pattern

---
+++ UniversalReportCore.Ui/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalReportCore.Ui
+{
+    public interface ICommand
+    {
+        bool CanExecute(object commandData);
+        Task ExecuteAsync(object commandData);
+    }
+}
---
+++ UniversalReportCore.Ui/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalReportCore.Ui
+{
+    public interface ICommandHandler
+    {
+        bool CanExecute(string commandName, object commandData);
+        Task ExecuteAsync(string commandName, object commandData);
+    }
+}
