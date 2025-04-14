f4f7d37155c4b91bd47f925cb23092e6fb5ca0ee
tonybierman <tonybierman@gmail.com>
2025-04-14 04:45:08 -0500

    IGetDisplayName

---
+++ UniversalReportCore/Helpers/EnumHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalReportCore.Helpers
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())[0]
+                .GetCustomAttribute<DisplayAttribute>()
+                ?.GetName() ?? enumValue.ToString();
+        }
+    }
+}
---
+++ UniversalReportCore/Helpers/IGetDisplayName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalReportCore.Helpers
+{
+    public interface IGetDisplayName
+    {
+        string GetDisplayName(int value);
+    }
+}
