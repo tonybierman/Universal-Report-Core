64fa271a422a7c022596fd9a2c6c78cf992369c4
tonybierman <tonybierman@gmail.com>
2025-08-21 14:46:05 -0500

    Added currency field value formatter

---
+++ UniversalReportCore.Ui/ViewModels/FieldFormatting/CurrencyFormatter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalReportCore.Ui.ViewModels.FieldFormatting
+{
+    public class CurrencyFormatter : IFieldFormatter
+    {
+        public bool CanHandle(object value, Type propertyType)
+        {
+            if (value != null)
+                return value is double || value is float || value is decimal;
+            return propertyType == typeof(double) || propertyType == typeof(double?) ||
+                   propertyType == typeof(decimal) || propertyType == typeof(decimal?) ||
+                   propertyType == typeof(float) || propertyType == typeof(float?);
+        }
+
+        public string Format(object value, Type propertyType)
+        {
+            return value != null ? string.Format("{0:C2}", value) : string.Format("{0:C2}", 0);
+        }
+    }
+}
