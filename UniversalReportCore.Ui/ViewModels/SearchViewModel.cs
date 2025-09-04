50988819ac1dc93c079e97951a9bff95409d0004
tonybierman <tonybierman@gmail.com>
2025-09-04 11:36:06 -0500

    Working on search filters

---
+++ UniversalReportCore.Ui/ViewModels/SearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversalReportCore.HardQuerystringVariables;
+
+namespace UniversalReportCore.Ui.ViewModels
+{
+    public class SearchViewModel
+    {
+        public IReportColumnDefinition[]? ReportColumns { get; set; }
+
+        public IReportQueryParams Params { get; set; }
+
+        public string? CurrentSort { get; set; }
+    }
+}
